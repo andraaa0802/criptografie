293208826e9a1fe34351d459092d08abc419555e
Vinter Andrada <vinterandrada08@gmail.com>
2022-11-21 18:02:19 +0200

    done

---
+++ PlayfairCipher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PlayfairCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce the text, please!");
+            string text = Console.ReadLine();
+            Console.WriteLine("Introduce the key, please!");
+            string key = Console.ReadLine();
+            Playfair playfair = new Playfair();
+            string encrypted = playfair.Encrypt(text,key);
+            Console.WriteLine("The encrypted text is: "+encrypted);
+            Console.WriteLine("The decrypted text is: " + playfair.Decrypt(encrypted, key));
+        }
+    }
+}
